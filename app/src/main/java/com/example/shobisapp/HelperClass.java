d5f6cedf651690cbe55eb2e310a3214ec677f709
Muhammad Haris Amin <muhammadharis.zero150@gmail.com>
2023-05-25 15:23:58 +0700

    Signup activity done!

--- app/src/main/java/com/example/shobisapp/HelperClass.java
+++ app/src/main/java/com/example/shobisapp/HelperClass.java
@@ -1,4 +1,48 @@
 package com.example.shobisapp;
 
 public class HelperClass {
+
+    String name, email, password, confirm_password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirm_password() {
+        return confirm_password;
+    }
+
+    public void setConfirm_password(String confirm_password) {
+        this.confirm_password = confirm_password;
+    }
+
+    public HelperClass(String name, String email, String password, String confirm_password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.confirm_password = confirm_password;
+    }
+
+    public HelperClass() {
+    }
 }
--- app/src/main/java/com/example/shobisapp/SignUpActivity.java
+++ app/src/main/java/com/example/shobisapp/SignUpActivity.java
@@ -2,13 +2,65 @@ package com.example.shobisapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    EditText nameEditText, emailEditText, passwordEditText, confirmpasswordEditText;
+    TextView loginTextView;
+    Button signupButton;
+    FirebaseDatabase database;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        nameEditText = findViewById(R.id.name_EditText);
+        emailEditText = findViewById(R.id.email_EditText);
+        passwordEditText = findViewById(R.id.password_EditText);
+        confirmpasswordEditText = findViewById(R.id.confirm_password_EditText);
+        signupButton = findViewById(R.id.signup_Button);
+        loginTextView = findViewById(R.id.login_TextView);
+
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                database = FirebaseDatabase.getInstance();
+                reference = database.getReference("users");
+
+                String name = nameEditText.getText().toString();
+                String email = emailEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+                String confirm_password = confirmpasswordEditText.getText().toString();
+
+                HelperClass helperClass = new HelperClass(name, email, password, confirm_password);
+                reference.child(name).setValue(helperClass);
+
+                Toast.makeText(SignUpActivity.this, "You have Signed Up Successfully!", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        loginTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
